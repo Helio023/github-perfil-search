fd7d49d47175bc57aeec87eac5959606d9745385
unknown <helioengracialipinze@gmail.com>
2022-03-25 11:17:00 -0700

    Button fixed

--- src/core/components/Button/AppButton.tsx
+++ src/core/components/Button/AppButton.tsx
@@ -4,7 +4,7 @@ type Props = {
   text: string;
   click?: (e: React.MouseEvent<HTMLButtonElement>) => void 
 };
-const AppButton = ({ text, click }: Props) => {
+const AppButton = ({ text, click}: Props) => {
   return <button type='button' className='btn' onClick={click}>{text}</button>;
 };
 
